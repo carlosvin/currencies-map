c7e76bd6e6bc3abeb17eccc50eb0f23430bf40f6
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-11-24 01:08:54 +0800

    project creation

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {'^.+\\.ts?$': 'ts-jest'},
+    testEnvironment: 'jsdom',
+    testRegex: '/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  };
+  
