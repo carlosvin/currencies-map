539f6d76e38b38dab9d66959b2eca9313e933ecf
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-11-24 18:46:07 +0800

    rename private var to underscore

--- index.ts
+++ index.ts
@@ -9,23 +9,24 @@ export class Currencies {
     static readonly codes = ["BDT", "EUR", "XOF", "BGN", "BAM", "BBD", "XPF", "BMD", "BND", "BOB", "BHD", "BIF", "BTN", "JMD", "BWP", "WST", "BRL", "BSD", "BYR", "BZD", "RUB", "RWF", "RSD", "USD", "TMT", "TJS", "RON", "NZD", "GTQ", "GBP", "XAF", "JPY", "GYD", "GEL", "GNF", "GMD", "GIP", "GHS", "OMR", "TND", "JOD", "HRK", "HTG", "HUF", "HKD", "HNL", "AUD", "VEF", "NOK", "PYG", "IQD", "PAB", "PGK", "PEN", "PKR", "PHP", "PLN", "ZMK", "EGP", "ZAR", "VND", "SBD", "ETB", "SOS", "ZWL", "SAR", "ERN", "MDL", "MGA", "MAD", "UZS", "MMK", "MOP", "MNT", "MKD", "MUR", "MWK", "MVR", "XCD", "MRO", "UGX", "TZS", "MYR", "MXN", "ILS", "SHP", "FJD", "FKP", "DKK", "NIO", "NAD", "VUV", "NGN", "NPR", "CHF", "COP", "CNY", "CLP", "CAD", "CDF", "CZK", "CRC", "ANG", "CVE", "CUP", "SZL", "SYP", "KGS", "KES", "SSP", "SRD", "KHR", "KMF", "STD", "KRW", "KPW", "KWD", "SLL", "SCR", "KZT", "KYD", "SGD", "SEK", "SDG", "DOP", "DJF", "YER", "DZD", "UYU", "LBP", "LAK", "TWD", "TTD", "TRY", "LKR", "TOP", "LTL", "LRD", "LSL", "THB", "LYD", "AED", "AFN", "ISK", "IRR", "AMD", "ALL", "AOA", "ARS", "AWG", "INR", "AZN", "IDR", "UAH", "QAR", "MZN"];
 
     private readonly _displayConfig: Intl.NumberFormatOptions;
+    private readonly _map = new Map<string, string>();
 
-    /** @param currencyDisplay Configure to show currency symbol or name */
+    /** 
+     * @param currencyDisplay Configure to show currency symbol or name 
+     * */
     constructor(currencyDisplay: 'symbol' | 'name') {
         this._displayConfig = { style: 'currency', currencyDisplay };
     }
-
-    private readonly map = new Map<string, string>();
 
     /**
      * @param code Three letters currency ISO code
      * @returns currency representation: name or symbol depending on instance configuration
      */
     getName(code: string) {
-        let name = this.map.get(code);
+        let name = this._map.get(code);
         if (name === undefined) {
             name = this._getName(code);
-            this.map.set(code, name);
+            this._map.set(code, name);
         }
         return name;
     }
